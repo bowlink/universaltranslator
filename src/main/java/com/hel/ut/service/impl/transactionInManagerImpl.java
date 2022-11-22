6e44e4571ff237c3738a4fa683cbcb491d80224d
Chad McCue <chadmccue@Chads-MacBook-Pro.local>
2022-11-22 11:05:47 -0500

    updated to alert if excel column mismatches

--- src/main/java/com/hel/ut/service/impl/transactionInManagerImpl.java
+++ src/main/java/com/hel/ut/service/impl/transactionInManagerImpl.java
@@ -201,9 +201,6 @@ public class transactionInManagerImpl implements transactionInManager {
     private emailMessageManager emailManager;
 
     @Autowired
-    private utilManager utilmanager;
-
-    @Autowired
     private zipFileManager zipFileManager;
 
     @Autowired
@@ -1838,20 +1835,8 @@ public class transactionInManagerImpl implements transactionInManager {
 
     @Override
     public void sendEmailToAdmin(String message, String subject) throws Exception {
-	try {
-	    mailMessage mail = new mailMessage();
-	    mail.setfromEmailAddress("support@health-e-link.net");
-	    mail.setmessageBody(message);
-	    mail.setmessageSubject(subject + " " + myProps.getProperty("server.identity"));
-	    mail.settoEmailAddress(myProps.getProperty("admin.email"));
-	    String[] ccEmailAddress = {myProps.getProperty("ccImport.email")};
-	    mail.setccEmailAddress(ccEmailAddress) ;
-	    
-	    emailManager.sendEmail(mail);
-	} catch (Exception ex) {
-	    ex.printStackTrace();
-	    throw new Exception(ex);
-	}
+    	 sendEmailToAdmin(message, subject, true);
+    			
     }
 
     @Override
@@ -2581,33 +2566,7 @@ public class transactionInManagerImpl implements transactionInManager {
 				//populate
 				populateAuditReport(batch.getId(), configurationManager.getMessageSpecs(batch.getConfigId()));
 				return;
-			} else if (newfilename.startsWith("Column Size Mismatch")) {
-				
-				List<configurationFormFields> configFormFields = configurationtransportmanager.getConfigurationFields(batch.getConfigId(), 0);
-
-		    	Integer totalFields = configFormFields.size();
-				
-		    	utConfiguration configDetails = new utConfiguration ();
-				if (batch.getConfigId() != 0) {
-					configDetails = configurationManager.getConfigurationById(batch.getConfigId());
-					
-			    }
-				
-			    //log batch activity
-			    ba = new batchuploadactivity();
-			    ba.setActivity(configDetails.getconfigName() + " - "+newfilename+" found in excel file.");
-			    ba.setBatchUploadId(batchId);
-			    transactionInDAO.submitBatchActivityLog(ba);
-			    
-			    updateBatchStatus(batchId, 7, "endDateTime");
-			    sendEmailToAdmin((new Date() + "<br/>Please login and review " + configDetails.getconfigName() + " file. "+newfilename+" found. Expecting  " + totalFields + " columns.  <br/>Batch Id -  " + batch.getId() + "<br/> UT Batch Name " + batch.getUtBatchName() + " <br/>Original batch file name - " + batch.getOriginalFileName()), "Column Sizes Mismatch");
-			    cleanAuditErrorTable(batch.getId());
-
-				//populate
-				populateAuditReport(batch.getId(), configurationManager.getMessageSpecs(batch.getConfigId()));
-				return;
-			}
-			else {
+			} else {
 			    //log batch activity
 			    ba = new batchuploadactivity();
 			    ba.setActivity("Successfully parsed the inbound Excel file and generated file location/name: " + decodedFilePath + newfilename);
@@ -5701,4 +5660,25 @@ public class transactionInManagerImpl implements transactionInManager {
     public List<batchUploads> getBatchesByOrgId(Integer orgId) throws Exception {
 	return transactionInDAO.getBatchesByOrgId(orgId);
     } 
+    
+    @Override
+    public void sendEmailToAdmin(String message, String subject, boolean sendToCC) throws Exception {
+		try {
+		    mailMessage mail = new mailMessage();
+		    mail.setfromEmailAddress("support@health-e-link.net");
+		    mail.setmessageBody(message);
+		    mail.setmessageSubject(subject + " " + myProps.getProperty("server.identity"));
+		    mail.settoEmailAddress(myProps.getProperty("admin.email"));
+		    if (sendToCC) {
+		    	String[] ccEmailAddress = {myProps.getProperty("ccImport.email")};
+		    	mail.setccEmailAddress(ccEmailAddress) ;
+		    }
+		    
+		    emailManager.sendEmail(mail);
+		} catch (Exception ex) {
+		    ex.printStackTrace();
+		    throw new Exception(ex);
+		}
+    }
+    
 }
