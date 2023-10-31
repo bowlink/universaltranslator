fa1d3c7186dec1770cd50892ffd1ee340d969acd
Chad McCue <chadmccue@Chads-MacBook-Pro.local>
2023-10-31 11:41:52 -0400

    move direct updates

--- src/main/java/com/hel/ut/restAPI/directManager.java
+++ src/main/java/com/hel/ut/restAPI/directManager.java
@@ -463,11 +463,6 @@ public class directManager {
 
                     if(!"".equals(accessToken)) {
                         
-                        ba = new batchdownloadactivity();
-                        ba.setActivity("Received medAllies access token: " + accessToken);
-                        ba.setBatchDownloadId(batchDownloadId);
-                        transactionOutDAO.submitBatchActivityLog(ba);
-
                         InputStream fileInput = new FileInputStream(file);
 
                         BufferedReader reader = new BufferedReader(new InputStreamReader(fileInput));
