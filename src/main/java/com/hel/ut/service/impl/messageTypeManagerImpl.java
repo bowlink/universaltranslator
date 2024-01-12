9de50e2d0cf8528c896493ed250e857491c6e002
Chad McCue <chadmccue@Chads-MacBook-Pro.local>
2024-01-12 15:11:56 -0500

    data translation updates
    added feature to upload multiple crosswalks for a configuration at once.

--- src/main/java/com/hel/ut/service/impl/messageTypeManagerImpl.java
+++ src/main/java/com/hel/ut/service/impl/messageTypeManagerImpl.java
@@ -360,6 +360,10 @@ public class messageTypeManagerImpl implements messageTypeManager {
 		executeSQLStatement("delete from rel_crosswalkData where crosswalkId = "+cwId);
 		executeSQLStatement("delete from crosswalks where Id = "+cwId);
             }
+            else {
+                executeSQLStatement("delete from rel_crosswalkData where crosswalkId = "+cwId);
+		executeSQLStatement("delete from crosswalks where Id = "+cwId);
+            }
 
         } catch (Exception e) {
             e.printStackTrace();
