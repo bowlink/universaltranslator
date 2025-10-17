4ce082ef53ff6f6a85f0a63252fd23574d31d2ab
Chad McCue <chadmccue05@gmail.com>
2025-10-17 16:47:02 -0400

    Hid fast processing
    When bill rate files are submitted they submit a demo and service file at the same time, this seems to cause lock failures sometimes on the batchUpload table causing one of the files not to process.

--- src/main/java/com/hel/ut/service/impl/transactionInManagerImpl.java
+++ src/main/java/com/hel/ut/service/impl/transactionInManagerImpl.java
@@ -1537,6 +1537,11 @@ public class transactionInManagerImpl implements transactionInManager {
                                                 }
                                                 else {
                                                     //Quick Processing Update
+                                                    //10-17-2025
+                                                    //CM: Turned this feature off, it seemed to be causing lock issues
+                                                    //on the batchUploads table when more bill rate files are submitted
+                                                    //for bill rate two files (Demo and Service) are uploaded together
+                                                    //and this seems to be an issue.
                                                     if(statusId == 42) {
                                                         loadMassBatches();
                                                     }
